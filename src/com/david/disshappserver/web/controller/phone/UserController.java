374e67bbe00ec50cf78a7fd3760c6bffb0182d21
qq986945193 <986945193@qq.com>
2017-08-19 12:47:52 +0800

    commit register

---
+++ src/com/david/disshappserver/web/controller/phone/UserController.java
@@ -0,0 +1,22 @@
+package com.david.disshappserver.web.controller.phone;
+
+import com.david.disshappserver.common.bean.ResponseInfo;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @Author ：程序员小冰
+ * @新浪微博 ：http://weibo.com/mcxiaobing
+ * @GitHub: https://github.com/QQ986945193
+ */
+@RequestMapping(value = "/phone/user")
+public class UserController {
+
+    /**
+     * 注册
+     */
+    @RequestMapping("/regist")
+    public ResponseInfo regist(){
+
+        return null;
+    }
+}
